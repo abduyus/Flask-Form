8ddba3badb259af59ae9c3a563fa8449590b4fc2
abduyus <yusuf.abdurrasheed@outlook.com>
2023-08-27 07:59:54 +0000

    getting user data from the HTML into Python

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/', methods=["GET", 'POST'])
 def index():
+    """index method"""
+    
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+        
     return render_template("index.html")
+
 
 app.run(debug=True, port=1406)
