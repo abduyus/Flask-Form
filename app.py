859ea2fe558c60d84e8d79ca84e72626bdf7f6ad
abduyus <yusuf.abdurrasheed@outlook.com>
2023-08-28 19:02:57 +0000

    Store user data in database & fixing context error

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "myapplication123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
 db = SQLAlchemy(app)
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 @app.route('/', methods=["GET", 'POST'])
 def index():
-    """index method"""
-    
     if request.method == "POST":
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         email = request.form["email"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, 
+            last_name=last_name, email=email, 
+            date=date_obj, occupation=occupation)
+        
+        db.session.add(form)
+        db.session.commit()
         
     return render_template("index.html")
 
-
-app.run(debug=True, port=1406)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=1406)
